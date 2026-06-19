4328242a680e299a87b1dc74ba7bda3bf31f8193
Pascal Brjansson <brjanssonp@gmail.com>
2026-06-19 12:42:46 +0200

    Clean up project structure

--- frontend/src/utils/modelSlug.ts
+++
@@ -1,7 +0,0 @@
-export const modelToSlug = (model: string): string =>
-  model
-    .toLowerCase()
-    .replace(/[()]/g, "")
-    .replace(/[^a-z0-9\s-]/g, "")
-    .trim()
-    .replace(/\s+/g, "-");
